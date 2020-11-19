378a406d72571c4f7666bf226512528e3d5efd64
pauljoyce <sdlqmc@yeah.net>
2020-11-19 20:29:59 +0800

    ClinitTest

---
+++ src/main/java/Runtime/ClinitTest.java
@@ -0,0 +1,19 @@
+package Runtime;
+
+public class ClinitTest {
+    int anInt;
+    private static int num = 1;
+    private static int number = 10;
+    static
+    {
+        number = 20;
+    }
+
+
+
+    public static void main(String[] args) {
+        System.out.println(number);
+
+    }
+}
+
